57edc7fbe06ec261015456504262f197d6fd364e
yoneshmurugan <yoneshmurugan@gmail.com>
2025-08-05 15:16:03 +0530

    Additional problems for Day 12

---
+++ Day 12/B_Three_Brothers.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class B_Three_Brothers{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int bro1 = scan.nextInt();
+        int bro2 = scan.nextInt();
+        System.out.println(6-(bro1 + bro2));
+    }
+}
